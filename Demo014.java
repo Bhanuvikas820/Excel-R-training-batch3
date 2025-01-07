c46a71be044bfdc597b0b503f8999d47c22b9b81
Bhanu Vikas Molugu <bhanuvikasmolugu@gmail.com>
2025-01-07 11:12:36 +0530

    Add files via upload

---
+++ Demo014.java
@@ -0,0 +1,16 @@
+public class Demo014 {
+    void Unaryoperations(){
+        int a = 8;
+        int b= 4;
+        System.out.println(++a);
+        System.out.println(--b);
+        System.out.println(a++);
+        System.out.println(b--);
+    }
+
+    public static void main(String[] args) {
+        Demo014 obj = new Demo014();
+        obj.Unaryoperations();
+    }
+
+}
