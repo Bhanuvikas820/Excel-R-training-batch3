0755eb4301b09e0e4164753645dafe6c026f4df3
Bhanu Vikas Molugu <bhanuvikasmolugu@gmail.com>
2025-01-08 11:38:24 +0530

    round off to nearest multiple of 5

---
+++ Demo019.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Demo019 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a two-digit number: ");
+        int number = scanner.nextInt();
+        
+        int roundedNumber = ((number + 2) / 5) * 5;
+        
+        System.out.println("Rounded to the nearest multiple of 5: " + roundedNumber);
+        
+        scanner.close();
+    }
+}
