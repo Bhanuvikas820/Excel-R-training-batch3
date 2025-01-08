317d8d14eac6f5bd564f587c276d2d86c4cc6577
Bhanu Vikas Molugu <bhanuvikasmolugu@gmail.com>
2025-01-08 11:28:00 +0530

    Minutes to Years

---
+++ Demo018.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Demo018 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter minutes: ");
+        long minutes = scanner.nextLong();
+        
+        long years = minutes / (60 * 24 * 365);
+        long days = (minutes % (60 * 24 * 365)) / (60 * 24);
+        
+        System.out.println(minutes + " minutes is approximately " + years + " years and " + days + " days.");
+        
+        scanner.close();
+    }
+}
