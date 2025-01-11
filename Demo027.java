719fc882ba60a2aa823b1f95e6ed2c5c40087f7a
Bhanu Vikas Molugu <bhanuvikasmolugu@gmail.com>
2025-01-11 11:49:05 +0530

    Add files via upload

---
+++ Demo027.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Demo027 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        if (isPrime(number)) {
+            System.out.println(number + " is a Prime number.");
+        } else {
+            System.out.println(number + " is a Composite number.");
+        }
+        sc.close();
+
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        if (n <= 3) {
+            return true;
+        }
+        if (n % 2 == 0 || n % 3 == 0) {
+            return false;
+        }
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
