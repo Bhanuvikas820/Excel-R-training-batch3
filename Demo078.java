4c4c1ccb3fb3e322ba1326345e77d426df881953
Bhanu Vikas Molugu <bhanuvikasmolugu@gmail.com>
2025-02-12 13:00:32 +0530

    print even numbers in left side and odd numbers in right

---
+++ Demo078.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Demo078 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }  
+        sc.close();
+        rearrangeEvenOdd(arr);
+        System.out.println("Array after rearranging even and odd numbers: " + Arrays.toString(arr));
+    }
+
+    static void rearrangeEvenOdd(int[] arr) {
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            while (arr[left] % 2 == 0 && left < right) {
+                left++;
+            }
+            while (arr[right] % 2 != 0 && left < right) {
+                right--;
+            }
+            if (left < right) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
